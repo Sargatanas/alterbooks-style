13b71ae863b0f1a6c8a76de885652ac0a705e41c
Silvamorta <sargatanas94@mail.ru>
2018-03-10 20:51:16 +0500

    Добавлена страница книги

---
+++ js/style/ripple.js
@@ -0,0 +1,38 @@
+let buttons = document.getElementsByClassName('button');
+
+Array.prototype.forEach.call(buttons, function (b) {
+    if ((b.tagName === 'BUTTON') && (b.dataset.type !== "simple")) {
+        if (b.dataset.default !== 'true') {
+            b.addEventListener('mousedown', createRipple);
+        }
+    } else {
+        b.classList.add('button_active');
+    }
+});
+
+function createRipple(e) {
+    let circle = document.createElement('div');
+    this.appendChild(circle);
+
+    let d = Math.max(this.clientWidth, this.clientHeight);
+
+    circle.style.width = circle.style.height = d + 'px';
+
+    circle.style.left = e.pageX - this.offsetLeft - d / 2 + 'px';
+    circle.style.top = e.pageY - this.offsetTop - d / 2 + 'px';
+
+    let date = new Date();
+    date = String(date.getTime());
+    circle.setAttribute('id', String(date));
+
+    setTimeout(function () {
+        let element = document.getElementById(date);
+        element.parentNode.removeChild(element);
+    }, 1000);
+
+
+    circle.classList.add('ripple');
+    if(this.dataset.color === 'light') {
+        circle.classList.add('ripple_light');
+    }
+}
