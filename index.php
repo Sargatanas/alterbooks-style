2628a3666f98fb2b4f398ad5ac09a987826b6292
Silvamorta <sargatanas94@mail.ru>
2018-03-31 01:18:46 +0500

    Добавлен профиль пользователя

--- index.php
+++ index.php
@@ -21,142 +21,14 @@
 
 <main class="container">
     <div class="row row-center">
-        <div class="book col-12">
+        <div class="col-6 col-md-8 col-sm-12">
             <div class="row">
-                <div class="book__aside col-4 col-md-0">
-                    <div class="row row-center">
-                        <div class="book__cover col-12"></div>
-                        <div class="book-buttons col-12">
-                            <div class="book-buttons__tab book-buttons__tab_3"></div>
-                            <div class="row">
-                                <button class="book-buttons__element button button_green col-12">
-                                    <span>читать</span>
-                                </button>
-                                <button class="book-buttons__element button col-12">
-                                    <span>в библиотеку</span>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="book-download col-8 col-center">
-                            <div class="row">
-                                <div class="book-download__title col-12 col-clear col-center">
-                                    Скачать
-                                </div>
-                                <button class="book-download__format button col-12 col-clear col-center">
-                                    <span>txt</span>
-                                </button>
-                                <div class="book-download__size col-12 col-clear col-center">
-                                    130Kb
-                                </div>
-
-                                <button class="book-download__format button col-12 col-clear col-center">
-                                    <span>epub</span>
-                                </button>
-                                <div class="book-download__size col-12 col-clear col-center">
-                                    1.01Mb
-                                </div>
-
-                                <button class="book-download__format button col-12 col-clear col-center">
-                                    <span>fb2</span>
-                                </button>
-                                <div class="book-download__size col-12 col-clear col-center">
-                                    301Kb
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="book__main col-8 col-md-12">
-                    <div class="row row-center">
-                        <div class="book__title col-12 col-clear col-md-center">
-                            Пиши, сокращай
-                        </div>
-                        <a class="book__author col-12 col-clear col-md-center" href="">
-                            Максим Ильяхов
-                        </a>
-                        <div class="book__cover col-0 col-clear col-md-12"></div>
-                        <div class="book-info col-12 col-clear col-md-11 col-md-center col-sm-12">
-                            <div class="book-info-element">
-                                <div class="book-info-element__main">
-                                    07.03.18
-                                </div>
-                                <div class="book-info-element__comment">
-                                    дата публикации
-                                </div>
-                            </div>
-                            <div class="book-info-element">
-                                <div class="book-info-element__main">
-                                    7.0
-                                </div>
-                                <div class="book-info-element__comment">
-                                    рейтинг
-                                </div>
-                            </div>
-                            <div class="book-info-element">
-                                <div class="book-info-element__main">
-                                    93
-                                </div>
-                                <div class="book-info-element__comment">
-                                    добавили в библиотеку
-                                </div>
-                            </div>
-                        </div>
-                        <div class="book__description col-12 col-clear col-md-11 col-sm-12">
-                            За две недели до сдачи макета я вдруг понял, что ОБЛОЖКУ-ТО МЫ ЗАБЫЛИ! Забыли напрочь:
-                            не обсудили, не задали вопросов, не задумались, — ничего. Ни издательство, ни мы с
-                            Людой понятия не имели, что должно быть на обложке. Только моя мама говорила, что череп
-                            на обложке — плохо.
-                            <br><br>
-                            С первого дня у нас был черновой вариант — сначала с черепом, потом по совету мамы —
-                            без. И где-то в глубине души я думал, что это и есть та самая, правильная обложка.
-                            В стиле Главреда: смело, радикально, просто...
-                        </div>
-                        <hr class="hr col-md-11 col-sm-12">
-                        <div class="book-genres col-12 col-clear col-md-11 col-sm-12">
-                            <div class="row">
-                                <div class="book-genres__title col-12 col-clear">
-                                    жанры
-                                </div>
-                                <div class="book-genres-content col-12 col-clear">
-                                    <a class="book-genres-content__element">документалистика</a>
-                                    <a class="book-genres-content__element">учебник</a>
-                                    <a class="book-genres-content__element">о жизни</a>
-                                    <a class="book-genres-content__element">приключения</a>
-                                    <a class="book-genres-content__element">мистика</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="book-buttons col-0 col-clear col-md-12 col-md-11 col-sm-12">
-                            <div class="row">
-                                <button class="book-buttons__element button button_green col-md-5 col-sm-12">
-                                    <span>читать</span>
-                                </button>
-                                <button class="book-buttons__element button col-md-5 col-sm-12">
-                                    <span>в библиотеку</span>
-                                </button>
-                            </div>
-                        </div>
-                        <hr class="hr col-0 col-md-11 col-sm-12">
-                        <div class="book-download col-0 col-clear col-md-12 col-md-11 col-sm-12">
-                            <div class="row">
-                                <div class="book-download__title col-12 col-clear">
-                                    Скачать
-                                </div>
-                                <button class="book-download__format button">
-                                    <span>txt</span>
-                                </button>
-
-                                <button class="book-download__format button">
-                                    <span>epub</span>
-                                </button>
-
-                                <button class="book-download__format button">
-                                    <span>fb2</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <a class="user-book__button button col-12" href="/pages/book.php">
+                    <span>профиль книги</span>
+                </a>
+                <a class="user-book__button button col-12" href="/pages/user.php">
+                    <span>профиль пользователя</span>
+                </a>
             </div>
         </div>
     </div>
---
+++ pages/user.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        User-profile
+    </title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/grid.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body class="body">
+
+<header class="header row row-center">
+    <div class="col-12 col-center col-middle">
+        He-e-e-e-e-ea-a-a-a-ader
+    </div>
+</header>
+
+<main class="container">
+    <div class="row row-center">
+        <div class="user col-12">
+            <div class="row">
+                <div class="user__aside col-4 col-md-0">
+                    <div class="row row-center">
+                        <div class="user__avatar col-12"
+                             style="background-image: url('/css/img/avatar.png')"></div>
+                        <div class="user-buttons col-12">
+                            <div class="row">
+                                <button class="user-buttons__element button col-12">
+                                    <span>библиотека</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="profile__main col-8 col-md-12">
+                    <div class="row row-center">
+                        <div class="user__name col-12 col-clear col-md-11 col-md-center col-sm-12">
+                            Пермякова Ксения Александровна
+                        </div>
+                        <div class="user__status col-12 col-clear col-md-11 col-md-center col-sm-12">
+                            была на сайте 02.03.2018
+                        </div>
+                        <div class="user__avatar col-0 col-clear col-md-8 col-sm-12"
+                             style="background-image: url('/css/img/avatar.png')"></div>
+                        <div class="block-info col-12 col-clear col-md-11 col-md-center col-sm-12">
+                            <div class="block-info-element">
+                                <div class="block-info-element__main">
+                                    7.0
+                                </div>
+                                <div class="block-info-element__comment">
+                                    рейтинг
+                                </div>
+                            </div>
+                            <div class="block-info-element">
+                                <div class="block-info-element__main">
+                                    11
+                                </div>
+                                <div class="block-info-element__comment">
+                                    книг написано
+                                </div>
+                            </div>
+                            <div class="block-info-element">
+                                <div class="block-info-element__main">
+                                    25
+                                </div>
+                                <div class="block-info-element__comment">
+                                    рецензий оставлено
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-0 col-clear col-md-11 col-sm-12">
+                            <div class="row">
+                                <button class="user-buttons__element button col-12">
+                                    <span>библиотека</span>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="user-content block-content col-12 col-clear col-md-11 col-sm-12">
+                            <div class="block-content-header">
+                                <div class="block-content-header__title">
+                                    данные&nbsp;пользователя
+                                </div>
+                                <hr class="block-content-header__hr">
+                            </div>
+                            <div class="block-content-main">
+                                <div class="block-content-main__element">
+                                    <div class="block-content-main__title">
+                                        Псевдоним
+                                    </div>
+                                    <div class="block-content-main__info">
+                                        Family@Cross
+                                    </div>
+                                </div>
+                                <div class="block-content-main__element">
+                                    <div class="block-content-main__title">
+                                        Дата рождения
+                                    </div>
+                                    <div class="block-content-main__info">
+                                        19.05.1993 (24 года)
+                                    </div>
+                                </div>
+                                <div class="block-content-main__element">
+                                    <div class="block-content-main__title">
+                                        Пол
+                                    </div>
+                                    <div class="block-content-main__info">
+                                        женский
+                                    </div>
+                                </div>
+                                <div class="block-content-main__element">
+                                    <div class="block-content-main__title">
+                                        Город
+                                    </div>
+                                    <div class="block-content-main__info">
+                                        Далеко-Дале-е-е-е-е-е-е-е-е-е-еко
+                                    </div>
+                                </div>
+                                <div class="block-content-main__element">
+                                    <div class="block-content-main__title">
+                                        О себе
+                                    </div>
+                                    <div class="block-content-main__info">
+                                        Лиса. Совсем лиса. Рыжая, бесхвостая, но все-равно лиса. Что уж поделать.
+                                        <br><br>
+                                        Рыжая, ну, почти рыжая. Кое-кто говорит, что наглая и борзая.
+                                        <br><br>
+                                        Что уж поделать?
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-center">
+                    <div class="user__books col-12 col-md-11 col-sm-12">
+                        <div class="col-12 col-clear">
+                            <div class="block-content row">
+                                <div class="col-12 col-clear col-md-0">
+                                    <div class="block-content-header block-content-header_center">
+                                        <div class="block-content-header__title">
+                                            книги&nbsp;автора
+                                        </div>
+                                        <hr class="block-content-header__hr">
+                                    </div>
+                                </div>
+                                <div class="col-0 col-clear col-md-12">
+                                    <div class="block-content-header">
+                                        <div class="block-content-header__title">
+                                            книги&nbsp;автора
+                                        </div>
+                                        <hr class="block-content-header__hr">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-clear">
+                                    <div class="row row-between">
+                                        <div class="user-book col-6 col-md-12 row">
+                                            <div class="user-book__main col-12">
+                                                <div class="user-book__cover"
+                                                     style="background-image: url('/css/img/cover.png')"></div>
+                                                <div class="user-book__info">
+                                                    <div class="user-book__title">
+                                                        Пиши, сокращай
+                                                    </div>
+                                                    <div class="block-content-main user-book-content">
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Опубликовано
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                07.03.18
+                                                            </div>
+                                                        </div>
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Страниц
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                128
+                                                            </div>
+                                                        </div>
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Рейтинг
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                7.0
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="user-book__footer col-12 col-md-10 col-sm-12">
+                                                <div class="row">
+                                                    <button class="user-book__button button button_green col-5 col-sm-12">
+                                                        <span>читать</span>
+                                                    </button>
+                                                    <button class="user-book__button button col-5 col-sm-12">
+                                                        <span>профиль</span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="user-book col-6 col-md-12 row">
+                                            <div class="user-book__main col-12">
+                                                <div class="user-book__cover"
+                                                     style="background-image: url('/css/img/cover.png')"></div>
+                                                <div class="user-book__info">
+                                                    <div class="user-book__title">
+                                                        Пиши, сокращай
+                                                    </div>
+                                                    <div class="block-content-main user-book-content">
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Опубликовано
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                07.03.18
+                                                            </div>
+                                                        </div>
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Страниц
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                128
+                                                            </div>
+                                                        </div>
+                                                        <div class="block-content-main__element user-book-content__element">
+                                                            <div class="block-content-main__title user-book-content__title">
+                                                                Рейтинг
+                                                            </div>
+                                                            <div class="block-content-main__info user-book-content__info">
+                                                                7.0
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="user-book__footer col-12 col-md-9 col-sm-12">
+                                                <div class="row">
+                                                    <button class="user-book__button button button_green col-5 col-sm-12">
+                                                        <span>читать</span>
+                                                    </button>
+                                                    <button class="user-book__button button col-5 col-sm-12">
+                                                        <span>профиль</span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="row row-end">
+                                                <button class="user-book__button button button_second col-4 col-md-12">
+                                                    <span>и еще 9 книг</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
