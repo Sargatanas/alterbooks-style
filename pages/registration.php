01f6db9976f400ef66cc76919b323ea6a51ec4c7
Silvamorta <sargatanas94@mail.ru>
2018-05-14 10:14:12 +0500

    Добавлена страница регистрации

--- pages/registration.php
+++ pages/registration.php
@@ -21,7 +21,127 @@
 
 <main class="container">
     <div class="row row-center">
+        <div class="registration col-8 col-clear col-md-11 col-md-clear">
+            <div class="row row-center">
+                <div class="registration__title col-12 col-clear col-center">
+                    регистрация
+                </div>
 
+
+                <div class="registration-element col-12 col-clear" data-status="correct">
+                    <div class="registration-element__logo"
+                         style="background-image: url('/css/img/icons/user_grey.svg')"></div>
+                    <input class="registration-element__field"
+                           type="text"
+                           id="login"
+                           name="login"
+                           placeholder="электронная почта / логин"
+                           required autofocus
+                           value="sargatanas@mail.ru">
+                    <div class="registration-element__flag"></div>
+                    <div class="registration-element__error">
+                        пользователь с таким логином существует
+                    </div>
+                    <div class="registration-element__star">*</div>
+                </div>
+
+                <div class="registration-element col-12 col-clear" data-status="">
+                    <div class="registration-element__logo"
+                         style="background-image: url('/css/img/icons/lock-closed_grey.svg')"></div>
+                    <input class="registration-element__field registration-element__field_logo"
+                           type="password"
+                           id="password"
+                           name="password"
+                           placeholder="пароль"
+                           required
+                           value="sargatanas@mail">
+                    <div class="registration-element__star">*</div>
+                </div>
+
+                <div class="registration-element col-12 col-clear" data-status="error">
+                    <div class="registration-element__logo"
+                         style="background-image: url('/css/img/icons/lock-closed_grey.svg')"></div>
+                    <input class="registration-element__field registration-element__field_logo"
+                           type="password"
+                           id="re-password"
+                           name="re-password"
+                           placeholder="повторите пароль"
+                           required
+                           value="sargatanas@mail.ru">
+                    <div class="registration-element__flag"></div>
+                    <div class="registration-element__error">
+                        пароли не совпадают
+                    </div>
+                    <div class="registration-element__star">*</div>
+                </div>
+
+                <div class="registration-element col-12 col-clear">
+                    <input class="registration-element__field"
+                           type="text"
+                           id="surname"
+                           name="surname"
+                           placeholder="фамилия">
+                </div>
+
+                <div class="registration-element col-12 col-clear">
+                    <input class="registration-element__field"
+                           type="text"
+                           id="name"
+                           name="name"
+                           placeholder="имя">
+                </div>
+                <div class="registration-element col-12 col-clear">
+                    <input class="registration-element__field"
+                           type="text"
+                           id="nickname"
+                           name="nickname"
+                           placeholder="псевдоним">
+                </div>
+
+
+                <div class="block-content col-12 col-clear">
+                    <div class="registration__hr block-content-header block-content-header_center">
+                        <hr class="block-content-header__hr">
+                        <div class="registration__separator block-content-header__title">
+                            или
+                        </div>
+                        <hr class="block-content-header__hr">
+                    </div>
+                    <div class="registration-more block-content-main">
+                        <div class="registration-more__element"
+                             style="background-image: url('/css/img/social/vk.svg')"></div>
+                        <div class="registration-more__element"
+                             style="background-image: url('/css/img/social/facebook.svg')"></div>
+                        <div class="registration-more__element"
+                             style="background-image: url('/css/img/social/twitter.svg')"></div>
+                        <div class="registration-more__element"
+                             style="background-image: url('/css/img/social/google.svg')"></div>
+
+                    </div>
+                </div>
+
+                <div class="col-12 col-clear col-center">
+                    <button class="registration__button button button_green">
+                        регистрация
+                    </button>
+                </div>
+
+                <div class="block-content col-12 col-clear">
+                    <div class="registration__hr block-content-header block-content-header_center">
+                        <hr class="block-content-header__hr">
+                        <div class="registration__separator block-content-header__title">
+                            уже&nbsp;зарегистрированы?
+                        </div>
+                        <hr class="block-content-header__hr">
+                    </div>
+                    <div class="registration-more block-content-main">
+                        <button class="registration__button button">
+                            авторизация
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
